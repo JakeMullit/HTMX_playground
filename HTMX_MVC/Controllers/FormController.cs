22b82b15349ceb500eec130e3e61776da8b4ec27
JakeMullit <jacob.moiniche@hotmail.com>
2024-02-18 22:22:03 +0100

    Add naive partial views

--- HTMX_MVC/Controllers/FormController.cs
+++ HTMX_MVC/Controllers/FormController.cs
@@ -1,4 +1,6 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using HTMX_MVC.Controllers.ControllerUtils;
+using HTMX_MVC.Models;
+using Microsoft.AspNetCore.Mvc;
 
 namespace HTMX_MVC.Controllers
 {
@@ -6,7 +8,38 @@ namespace HTMX_MVC.Controllers
     {
         public IActionResult Index()
         {
-            return View();
+            var model = HttpContext.GetCreateModel<FormModel>();
+            return View(model);
+        }
+
+        [HttpPut]
+        public IActionResult InputName(string name)
+        {
+            var model = HttpContext.GetCreateModel<FormModel>();
+            model.Name = name;
+            HttpContext.SaveModel(model);
+
+            return PartialView(model);
+        }
+
+        [HttpPut]
+        public IActionResult InputEmail(string email)
+        {
+            var model = HttpContext.GetCreateModel<FormModel>();
+            model.Email = email;
+            HttpContext.SaveModel(model);
+
+            return PartialView(model);
+        }
+
+        [HttpPut]
+        public IActionResult InputPassword(string password)
+        {
+            var model = HttpContext.GetCreateModel<FormModel>();
+            model.Password = password;
+            HttpContext.SaveModel(model);
+
+            return PartialView(model);
         }
     }
 }
---
+++ HTMX_MVC/Models/FormModel.cs
@@ -0,0 +1,10 @@
+﻿namespace HTMX_MVC.Models
+{
+    public class FormModel
+    {
+        public string Name { get; set; } = "";
+        public string Email { get; set; } = "";
+        public string Password { get; set; } = "";
+        public bool HasAgreedToTerms { get; set; } = false;
+    }
+}
